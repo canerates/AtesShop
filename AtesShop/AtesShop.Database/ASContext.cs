a5d57e70d9c8b9b107008c5728ea64122c3e05c1
Caner <cnrates@gmail.com>
2019-12-16 13:38:16 +0800

    Added Product and Category Business Entities. Created Database.

---
+++ AtesShop/AtesShop.Database/ASContext.cs
@@ -0,0 +1,22 @@
+﻿using AtesShop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtesShop.Database
+{
+    public class ASContext : DbContext
+    {
+        public ASContext() : base("AtesShopConnection")
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Properties { get; set; }
+
+    }
+}
